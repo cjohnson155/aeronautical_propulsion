1538ba95c250ec550c7f7e84e6554dba75dc2373
cjohnson155 <110058634+cjohnson155@users.noreply.github.com>
2026-06-30 06:42:58 -0700

    Delete Unit2 directory

--- Unit2/App.jsx
+++
@@ -1,8 +0,0 @@
-// ME 3470 . Unit 2 - entry point
-import Presentation from './engine'
-import { slides, sections } from './slides'
-import { meta } from './meta'
-
-export default function App() {
-  return <Presentation slides={slides} meta={meta} sections={sections} />
-}
